b3c5c2b41ce4124b5a1c2f590ae84c50acf315d5
jango100500-ui <jango100500@gmail.com>
2026-07-31 11:58:28 +0300

    Update and rename tg.js to tg.ts

--- src/lib/tg.ts
+++ src/lib/tg.ts
@@ -1,3 +1,11 @@
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: any
+    }
+  }
+}
+
 export const getTg = () => {
   if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
     return window.Telegram.WebApp
