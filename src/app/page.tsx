8458048b1ba7722c8ee0369e2608b0755f9ae806
jango100500-ui <jango100500@gmail.com>
2026-07-31 12:55:45 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import React, { useEffect } from 'react'
 import { TopBar } from '@/ui/topbar'
+import { initTelegramRules } from '@/lib/tg'
 
 export default function Home() {
+  useEffect(() => {
+    initTelegramRules()
+  }, [])
+
   return (
     <>
       <TopBar />
