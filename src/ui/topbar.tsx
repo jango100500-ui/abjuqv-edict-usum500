db785fa9837d27cec2c0bfa863f486023c391612
jango100500-ui <jango100500@gmail.com>
2026-07-31 12:46:29 +0300

    Create topbar.tsx

---
+++ src/ui/topbar.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+
+export const TopBar = () => {
+  return (
+    <header className="topheader">
+      <div className="logowrap">
+        <img 
+          src="/logos/cardslogo.png" 
+          alt="sw cardsbot logo" 
+          className="mainlogo"
+        />
+      </div>
+    </header>
+  )
+}
