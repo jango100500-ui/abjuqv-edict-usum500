c8deb54fff1bc2356c35709ccb5c004a353a0fae
jango100500-ui <jango100500@gmail.com>
2026-07-31 12:47:39 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { TopBar } from '@/ui/topbar'
+
 export default function Home() {
   return (
-    <div className="testbox">
-      test
-    </div>
+    <>
+      <TopBar />
+      <main className="testcontent">
+        test
+      </main>
+    </>
   )
 }
