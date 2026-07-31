9a3bbca2c2373e970da77f5f1219852bb23d7389
jango100500-ui <jango100500@gmail.com>
2026-07-31 14:15:25 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TopBar } from '@/ui/topbar'
+import { TabBar } from '@/ui/tabbar'
 import { initTelegramRules } from '@/lib/tg'
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<string>('cards')
+
   useEffect(() => {
     initTelegramRules()
   }, [])
@@ -12,9 +15,16 @@ export default function Home() {
   return (
     <>
       <TopBar />
+      
       <main className="testcontent">
-        test
+        {activeTab === 'market' && <div>Раздел: Рынок</div>}
+        {activeTab === 'leaders' && <div>Раздел: Лидеры</div>}
+        {activeTab === 'cards' && <div>Раздел: Карты</div>}
+        {activeTab === 'tasks' && <div>Раздел: Задания</div>}
+        {activeTab === 'profile' && <div>Раздел: Профиль</div>}
       </main>
+
+      <TabBar activeTab={activeTab} setActiveTab={setActiveTab} />
     </>
   )
 }
