549da7361cdf51e1a9e37b72da96bef430e1544f
jango100500-ui <jango100500@gmail.com>
2026-07-31 20:21:33 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import { TopBar } from '../ui/topbar'
 import { TabBar } from '../ui/tabbar'
+import { HomeHeader } from '../ui/main/home-header'
 import { initTelegramRules } from '../lib/tg'
 
 export default function Home() {
@@ -14,12 +15,14 @@ export default function Home() {
 
   return (
     <>
+      {activeTab === 'cards' && <div className="home-top-glow" />}
+
       <TopBar />
       
-      <main className="testcontent">
+      <main className="pagebody">
+        {activeTab === 'cards' && <HomeHeader />}
         {activeTab === 'market' && <div>Раздел: Рынок</div>}
         {activeTab === 'leaders' && <div>Раздел: Лидеры</div>}
-        {activeTab === 'cards' && <div>Раздел: Карты</div>}
         {activeTab === 'tasks' && <div>Раздел: Задания</div>}
         {activeTab === 'profile' && <div>Раздел: Профиль</div>}
       </main>
